f0ed445afd3232d81f5827c4bf959b029da5230a
SarahLcf-Tetia <sarah.lacoffrette@tetia.com>
2023-05-20 09:13:26 +0200

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Sarah Lacoffrette on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
